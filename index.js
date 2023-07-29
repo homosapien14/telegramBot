0479934cc5e8c17b3ce8c4b4bf4c132bee5c4cb3
homosapien14 <spma259@gmail.com>
2023-07-29 15:57:01 +0530

    added project

---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.command('blockchain', (ctx)=>ctx.reply('Blockchain'))
+bot.launch();
