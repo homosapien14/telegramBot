aafaffeedd74985729cafcb7c854618409bc294e
homosapien14 <spma259@gmail.com>
2023-07-29 15:58:50 +0530

    fix

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
-bot.command('blockchain', (ctx)=>ctx.reply('Blockchain'))
+bot.command('blockchain', (ctx)=>ctx.reply('Blockchain'));
+
+bot.on('sticker', (ctx) => ctx.reply(''))
 bot.launch();
